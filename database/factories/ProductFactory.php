a4a9f6629281fd3016277debfa4916ce97e21435
Hesam Khaki <nk.hesam@gmail.com>
2021-03-17 13:56:52 +0330

    Add product model and its factory and seeders

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->word,
+            'price' => $this->faker->randomElement([10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]),
+        ];
+    }
+}
