4d43c450eb3b24ffa2adfc5f74a0a36fcf388151
Hesam Khaki <nk.hesam@gmail.com>
2021-03-17 17:35:34 +0330

    Add create, update and view APIs for Product model

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
+class ProductController extends Controller
+{
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Validator $validator)
+    {
+        if (($validator = $this->productRequestValidator($request))->fails()){
+            return response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+
+        return response()->json(Product::create($request->all()), Response::HTTP_CREATED);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        if (($validator = $this->productRequestValidator($request))->fails()){
+            return response()->json($validator->errors(), Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $product->update($request->all());
+
+        return $product;
+    }
+
+    protected function productRequestValidator(Request $request)
+    {
+        return Validator::make($request->all(), [
+            'name' => 'required|max:255',
+            'price' => 'required|numeric|between:10,500',
+        ]);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,9 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['name', 'price'];
+
+
     public function deals()
     {
         return $this->hasMany(Deal::class);
